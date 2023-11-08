a524972c7cf4f7d4b3924aef323523e3ef0fc3ce
MelvinMartins-NIST <114179002+MelvinMartins-NIST@users.noreply.github.com>
2023-11-08 10:05:52 -0500

    Update entrypoint.sh

--- fm/entrypoint.sh
+++ fm/entrypoint.sh
@@ -3,4 +3,4 @@
 BRANCH_NAME="standalone"
 
 git clone --branch $BRANCH_NAME https://github.com/usnistgov/nextcloud-api
-git clone --branch $BRANCH_NAME https://github.com/usnistgov/fm-application-layer
+git clone --branch $BRANCH_NAME https://github.com/usnistgov/oar-fm-application-layer
